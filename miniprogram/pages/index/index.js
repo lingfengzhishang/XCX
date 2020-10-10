1ee02cc2b06356c8f8b5bdb473fd180fed53d472
凌风 <48054578+lingfengzhishang@users.noreply.github.com>
2020-10-10 17:23:57 +0800

    10.10

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,7 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-    "adImg":[]
+    "adImg":[],
+    "goodNew":[],
+    "goodHot":[],
+    "goodRecommend":[],
+    "goods":[],
+    showloding: true
   },
   /**
    * 生命周期函数--监听页面加载
@@ -13,6 +18,10 @@ Page({
   
   onLoad: function (options) {
     this.getAd()
+    this.getNew()
+    this.getHot()
+    this.getRecommend()
+    this.getList()
   },
 
   /**
@@ -54,7 +63,14 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    if(this.data.showloding){
+      this.getList()
+    }else{
+      wx.showToast({
+        title: '暂无数据',
+        icon: 'none'
+      })
+    }
   },
 
   /**
@@ -73,5 +89,63 @@ Page({
         })
       }
     })
+  },
+  getGoodsCate: function(id){
+    var that = this
+    wx.request({
+      url: 'https://www.lingfengzhishang.cn/tp_lsj/public/index.php/api/ad/getGoodsCate/id/'+id,
+      success: function(res){
+        switch (id) {
+          case 1:
+            that.setData({
+              goodRecommend:res.data.data
+            })
+              break;
+          case 2:
+            that.setData({
+              goodNew:res.data.data
+            })
+              break;
+          case 3:
+            that.setData({
+              goodHot:res.data.data
+            })
+              break;
+        }
+      }
+    })
+  },
+  getList: function(){
+    var that = this
+
+    wx.showLoading({
+      title: '加载中...',
+    })
+
+    wx.request({
+      url: 'https://www.lingfengzhishang.cn/tp_lsj/public/index.php/api/ad/getList/page/'+ that.data.goods.length,
+      success: function(res){ 
+        if(res.data.data.length == 0){
+          that.setData({
+            showloding : false
+          })
+        }else{
+          that.setData({
+            goods: that.data.goods.concat(res.data.data)
+          })
+        }
+  
+        wx.hideLoading()
+      }
+    })
+  },
+  getNew:function(){
+    this.getGoodsCate(2)
+  },
+  getHot:function(){
+    this.getGoodsCate(3)
+  },
+  getRecommend:function(){
+    this.getGoodsCate(1)
   }
 })
