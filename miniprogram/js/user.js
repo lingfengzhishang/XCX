b582e855a7b6dc0e539d0c8887628c2bca2f567a
lingfengzhishang <lingfengzhishang@gmail.com>
2020-10-12 01:55:40 +0800

    10.12

---
+++ miniprogram/js/user.js
@@ -0,0 +1,14 @@
+
+/**
+ * ES6的语法  
+ * export 后面的内容能被导出
+ * import 后面能引入要导出的文件
+ * 
+ * 
+ * export function 方法名（）{} 
+ * 
+ * import {方法名, 方法名} from '相对路径'
+ */
+export function getUserInfo(){
+  console.log('成功了')
+}
--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,20 +1,104 @@
-// pages/home/home.js
+
+import {getUserInfo} from '../../js/user.js'
+// pages/profile/profile.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    iShidden: false,
+    userInfo: []
   },
 
+//点击授权  弹窗提示
+  goPages:function(){
+      this.setData({ iShidden: true })
+  },
+
+  //点击在想想
+  onClose:function(){
+    getUserInfo()
+    this.setData({ iShidden: false })
+  },
+
+  //点击授权按钮  
+  bindGetUserInfo:function(event){
+      var that = this
+      // 查询一下用户是否授权
+      wx.getSetting({
+        success(res) {
+          if(res.authSetting['scope.userInfo']){
+            //授权了
+            // 必须是在用户已经授权的情况下调用
+            wx.getUserInfo({
+              success: function(res) {
+                that.setData({
+                  userInfo : res.userInfo,
+                  iShidden: false
+                })
+                wx.login({
+                  success (loginres) {
+                    if (loginres.code) {
+                      //发起网络请求
+                      wx.request({
+                        url: 'https://www.lingfengzhishang.cn/tp_lsj/public/index.php/api/login/getOpenId/code/'+loginres.code+'/name/'+that.data.userInfo.nickName+'/img/'+that.data.userInfo.avatarUrl,
+                        success: function(successres){
+                            console.log(successres)
+                        }
+                      })
+                    } else {
+                      console.log('登录失败！' + res.errMsg)
+                    }
+                  }
+                })
+              }
+            })
+          }else{
+            wx.authorize({
+              scope: 'scope.userInfo',
+              success: function(res){
+                //授权
+              }
+            })
+          }
+        }
+      })
+     
+  },
+
+  // getUserInfos:function(){
+  //   //登录提示
+  //   wx.showLoading({
+  //     title: '登陆中...',
+  //   })
+  //   var that = this
+  //   wx.login({
+  //     success (res) {
+  //       if (res.code) {
+  //         //发起网络请求
+  //         wx.request({
+  //           url: 'https://www.lingfengzhishang.cn/tp_lsj/public/index.php/api/login/getuserinfo/code/'+res.code,
+  //           success: function(res){
+  //               that.setData({
+  //                 userInfo:res.data.data
+  //               })
+  //               wx.hideLoading( )
+  //           }
+  //         })
+  //       } else {
+  //         console.log('登录失败！' + res.errMsg)
+  //       }
+  //     }
+  //   })
+  // },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getUserInfos();
   },
-
+ 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
